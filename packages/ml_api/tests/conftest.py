b6456353025090e333abec632406533b9bff0c6e
Jérôme Nadeau <2197696@csfoy.ca>
2024-12-30 15:11:04 -0500

    10.Ajout-prédiction-test à notre Flask api

--- packages/ml_api/tests/conftest.py
+++ packages/ml_api/tests/conftest.py
@@ -1,20 +1,5 @@
 import pytest
 
-import os
-import sys
-
-# def add_parent_directory_to_path():
-#     parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#     sys.path.append(parent_dir)
-
-def add_subdirectories_to_path(rootdir):
-    for dirpath, dirnames, filenames in os.walk(rootdir):
-        sys.path.append(dirpath)
-
-rootdir = os.path.dirname(os.path.abspath(__file__))
-# add_parent_directory_to_path()
-add_subdirectories_to_path(rootdir)
-
 from api.app import create_app
 from api.config import TestingConfig
 
